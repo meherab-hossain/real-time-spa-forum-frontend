f766565e6b5ba9e8dabd68c3886d2a651de177d2
Md. Meherab Hossain <38518474+meherab-hossain@users.noreply.github.com>
2019-12-09 11:24:38 +0600

    navigation drawer added

--- src/components/Toolbar.vue
+++ src/components/Toolbar.vue
@@ -1,7 +1,8 @@
 <template>
     <div>
         <v-card height="60px" flat tile color="gray-light-4">
-            <v-toolbar dark color="indigo darken-1">
+            <v-toolbar dark color="indigo darken-1" class="header">
+                <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
                 <v-toolbar-title>
                     <router-link to="/">Forum Page</router-link>
                 </v-toolbar-title>
@@ -26,6 +27,55 @@
                     </template>
                 </div>
             </v-toolbar>
+            <v-sheet
+                    height="465"
+                    class="overflow-hidden"
+                    style="position: relative"
+            >
+                <v-navigation-drawer
+                        v-model="drawer"
+                        absolute
+                        temporary
+                >
+                    <v-list-item>
+                        <v-list-item-avatar>
+                            <v-img src="https://randomuser.me/api/portraits/men/78.jpg"></v-img>
+                        </v-list-item-avatar>
+
+                        <v-list-item-content>
+                            <v-list-item-title>{{this.userName()}}</v-list-item-title>
+                        </v-list-item-content>
+                    </v-list-item>
+
+                    <v-divider></v-divider>
+
+                    <v-list dense>
+                        <div>
+                            <template v-if="isLogged">
+                                <v-list-item v-for="item in authMenu" :key="item.title" :to="item.to">
+                                    <v-list-item-icon class="darken-1">
+                                        <v-icon>{{ item.icon }}</v-icon>
+                                    </v-list-item-icon>
+                                    <v-list-item-content>
+                                        <v-list-item-title>{{ item.title }}</v-list-item-title>
+                                    </v-list-item-content>
+                                </v-list-item>
+
+                            </template>
+                            <template v-if="!isLogged">
+                                <v-list-item v-for="item in unAuthMenu" :key="item.title" :to="item.to">
+                                    <v-list-item-icon>
+                                        <v-icon>{{ item.icon }}</v-icon>
+                                    </v-list-item-icon>
+                                    <v-list-item-content>
+                                        <v-list-item-title>{{ item.title }}</v-list-item-title>
+                                    </v-list-item-content>
+                                </v-list-item>
+                            </template>
+                        </div>
+                    </v-list>
+                </v-navigation-drawer>
+            </v-sheet>
         </v-card>
     </div>
 </template>
@@ -39,19 +89,19 @@
     export default {
         name: "AppHome",
         components: {AppNotification},
-        mixins: [User,AppStorage],
+        mixins: [User, AppStorage],
         data() {
             return {
-                menus: [],
+                drawer: null,
                 authMenu: [
-                    {title: 'forum', to: '/forum'},
-                    {title: 'Ask Question', to: '/ask'},
-                    {title: 'category', to: '/category'},
-                    {title: 'logout', to: '/logout'}
+                    {title: 'forum', to: '/forum', icon: 'dashboard'},
+                    {title: 'Ask Question', to: '/ask', icon: 'question_answer'},
+                    {title: 'category', to: '/category', icon: 'list'},
+                    {title: 'logout', to: '/logout', icon: 'logout'}
                 ],
                 unAuthMenu: [
-                    {title: 'forum', to: '/forum'},
-                    {title: 'login', to: '/login'}
+                    {title: 'forum', to: '/forum',icon: 'dashboard'},
+                    {title: 'login', to: '/login',icon: 'logout'}
                 ],
                 isLogged: this.checkIfIsLogged()
             }
@@ -59,7 +109,7 @@
         created() {
             EventBus.$on('logout', () => {
                 this.loggedOut()
-                this.isLogged=this.checkIfIsLogged()
+                this.isLogged = this.checkIfIsLogged()
                 this.$router.push('/forum')
             }),
                 EventBus.$on('login', () => {
@@ -72,7 +122,7 @@
                 if (this.getToken()) {
                     return true
                 } else {
-                    return  false
+                    return false
                 }
             }
         }
@@ -81,5 +131,14 @@
 </script>
 
 <style scoped>
-
+.header{
+    position:fixed; /* fixing the position takes it out of html flow - knows
+                   nothing about where to locate itself except by browser
+                   coordinates */
+    left:0;           /* top left corner should start at leftmost spot */
+    top:0;            /* top left corner should start at topmost spot */
+    width:100vw;      /* take up the full browser width */
+    z-index:200;  /* high z index so other content scrolls underneath */
+    height:100px;     /* define height for content */
+}
 </style>
